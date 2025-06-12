bfd4ee446e96316f1b3a9f7f75631239cbc45d88
AndreJacobino <andre.jacobino@aluno.senai.br>
2025-06-12 17:13:13 -0300

    conometro final

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
 let segundos = 0;
+let mile = 0
 let intervalo = null;
 
-const display = document.getElementById("display");
-const iniciar = document.getElementById("iniciar");
-const  pausar = document.getElementById("pausar");
-const resetar = document.getElementById("resetar");
+const display = document.getElementById('display');
+const iniciarBtn = document.getElementById('iniciar');
+const pausarBtn = document.getElementById('pausar');
+const resetarBtn = document.getElementById('reset');
+const trocaTema = document.getElementById('trocaTema')
 
-iniciar.addEventListener("click", inicio)
-pausar.addEventListener("click", pausa)
-resetar.addEventListener("click", reseta)
+iniciarBtn.addEventListener("click", start);
+pausarBtn.addEventListener("click", pause);
+resetarBtn.addEventListener("click", reset);
+trocaTema.addEventListener("click", trocatema);
 
-function inicio(){
-    if(intervalo) return;
+// começar cronometro
+function start() {
+    if (intervalo) return;
 
-    intervalo = setInterval(()=>{
-        segundos++;
+    intervalo = setInterval(() => {
+        mile += 10;
+        if (mile >= 1000) {
+            mile = 0;
+            segundos++;
+        }
         atualizaDisplay();
 
-    },1000);
-
+        
+        
+    }, 10);
 }
 
-function pausa(){
+// pausar cronometro
+function pause() {
     clearInterval(intervalo);
     intervalo = null
-
-
-}
-
-function reseta(){
-    pausa();
-    segundos = 0;
-    atualizaDisplay();
 
     
 }
 
-function formatarTempo(segundosTotais){
-    const minutos = Math.floor(segundosTotais/60);
+// resetar cronometro
+function reset() {
+    pause();
+    segundos = 0;
+    mile = 0;
+    atualizaDisplay();
+
+
+}
+
+
+// formatação de tempo
+function formatarTempo(segundosTotais) {
+    const minutos = Math.floor(segundosTotais / 60)
     const segundos = segundosTotais % 60;
-    return `${String(minutos).padStart(2,'0')}: ${String(segundos).padStart(2,'0')}`
+    const ms = Math.floor(mile /10)
+    return` ${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}:${String(ms).padStart(2, '0')}`
 
 }
 
-function atualizaDisplay(){
-    display.textContent = formatarTempo(segundos);
+function atualizaDisplay() {
+    display.textContent = formatarTempo(segundos, mile);
 }
+
+const botao = document.getElementById("trocaTema")
+
+function trocatema(){
+    const cores = [
+        "#ffffff",
+        "#000000"
+    ]   
+        const indiceAleatorio = Math.floor(Math.random()* cores.length);
+        return cores[indiceAleatorio]
+}
+
+botao.addEventListener("click", function(){
+    document.body.style.background = trocatema()
+});
+
